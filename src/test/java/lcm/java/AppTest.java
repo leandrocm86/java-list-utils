825c56586bf16e4f1b8cfc8f6a5368bf277e3a78
Leandro Medeiros <leandrocm86@gmail.com>
2022-07-19 20:49:46 -0300

    first commit

---
+++ src/test/java/lcm/java/AppTest.java
@@ -0,0 +1,20 @@
+package lcm.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
